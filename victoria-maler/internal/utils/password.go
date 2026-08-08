26123daaa1e67f16033d9a5ccc8e42976482dea9
flovatieno <odhiambo.flovi@gmail.com>
2026-08-08 16:06:53 +0300

    feat(utils): bcrypt password hashing and verification

--- victoria-maler/internal/utils/password.go
+++ victoria-maler/internal/utils/password.go
@@ -1,3 +1,17 @@
 package utils
 
-// Password helpers placeholder
+import "golang.org/x/crypto/bcrypt"
+
+const bcryptCost = 12
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcryptCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func CheckPassword(hash, password string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) == nil
+}
