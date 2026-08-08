89d2e2f370f5ce2401f6cef479720b41309e222c
flovatieno <odhiambo.flovi@gmail.com>
2026-08-08 16:06:42 +0300

    feat(utils): hex-encoded sha256 hashing helpers

--- victoria-maler/go.mod
+++ victoria-maler/go.mod
@@ -3,3 +3,8 @@ module ecochain-victoria
 go 1.20
 
 require github.com/mattn/go-sqlite3 v1.14.17
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+)
--- victoria-maler/internal/utils/hash.go
+++ victoria-maler/internal/utils/hash.go
@@ -1,8 +1,20 @@
 package utils
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
 
 func Sum(data string) []byte {
-    h := sha256.Sum256([]byte(data))
-    return h[:]
+	h := sha256.Sum256([]byte(data))
+	return h[:]
+}
+
+func HashHex(data string) string {
+	return hex.EncodeToString(Sum(data))
+}
+
+func HashHexBytes(data []byte) string {
+	h := sha256.Sum256(data)
+	return hex.EncodeToString(h[:])
 }
