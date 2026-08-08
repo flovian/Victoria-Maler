8ae339bac8c80e52f983be3c547a0d029df16d2b
flovatieno <odhiambo.flovi@gmail.com>
2026-08-08 16:26:25 +0300

    feat(handlers): home and about page handlers

--- victoria-maler/internal/handlers/home_handler.go
+++ victoria-maler/internal/handlers/home_handler.go
@@ -1,3 +1,38 @@
 package handlers
 
-// Home handler placeholder
+import (
+	"net/http"
+
+	"ecochain-victoria/internal/utils"
+)
+
+func (h *HandlerSet) Home(w http.ResponseWriter, r *http.Request) {
+	campaigns, err := h.Campaigns.ListCampaigns()
+	if err != nil {
+		utils.InternalError(w, "could not load campaigns")
+		return
+	}
+
+	activeCount := 0
+	for _, c := range campaigns {
+		if c.Status == "active" {
+			activeCount++
+		}
+	}
+
+	totalRaised := 0.0
+	for _, c := range campaigns {
+		totalRaised += c.RaisedAmount
+	}
+
+	h.Render.Page(w, r, "home", map[string]interface{}{
+		"Campaigns":     campaigns,
+		"ActiveCount":   activeCount,
+		"CampaignCount": len(campaigns),
+		"TotalRaised":   totalRaised,
+	})
+}
+
+func (h *HandlerSet) About(w http.ResponseWriter, r *http.Request) {
+	h.Render.Page(w, r, "about", nil)
+}
